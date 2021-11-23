3946eaf7788e6f76d52e24db2237ce7486045e46
SpecsNStats <specsnstats@gmail.com>
2021-11-23 14:33:12 -0700

    installing more packages for MUI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import SignupForm from "./components/SignupForm/index.js"
+import React from "react";
+import ReactDOM from "react-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <h1>Hello World!</h1>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      
+    </>
   );
 }
 
