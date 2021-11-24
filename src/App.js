a33825871b16789917d2fc05a109cbeb6259b9cc
Charlotte Hulseman <chulseman@fordham.edu>
2021-11-24 14:46:39 -0500

    profile page mockup work in app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import SignupForm from "./components/SignupForm/index.js"
-import EditProfile from "./components/MyProfile/index.js"
+import Profile from "./pages/Profile"
 import React from "react";
 import ReactDOM from "react-dom";
+import Avatar from "@mui/material/Avatar"
+import Box from '@mui/material/Box';
+import Chip from '@mui/material/Chip';
+
 
 
 function App() {
   return (
     <>
       <SignupForm/>
+
+
+      <h1>
+      --------------------------------
+      </h1>
+<Box>
+      <h1> @charlotte </h1>
+<Avatar
+  alt="Panda"
+  src="./public/images/panda.jpg"
+  sx={{ width: 150, height: 150}}
+/>
+<h1> 33 votes </h1>
+<Chip label="Pet Badge" variant="outlined"
+sx={{ width: 100, height: 30}}/>
+<h1> Favorite Pet: Panda </h1>
+</Box>
+
+
+
     </>
   );
 }
