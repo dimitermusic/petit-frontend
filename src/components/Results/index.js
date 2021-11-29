99a8e5bf4a5fcdfaa8ebb087f8d2413b2cd8176c
kelliekumasaka <kelliek3@gmail.com>
2021-11-29 11:35:04 -0800

    results component changes

--- src/components/Results/index.js
+++ src/components/Results/index.js
@@ -2,7 +2,15 @@ import React, { useState } from "react";
 import PropTypes from 'prop-types';
 
 function Results(props){
-
+    return(
+        <>
+            <div className="uk-container uk-width-4-5">
+                <h1 className="uk-heading-divider uk-text-center">Search Results</h1>
+                <ul className="uk-list uk-list-large uk-list-divider uk-list-striped" id='search-results'>
+                </ul>
+            </div>
+        </>
+    )
 }
 
 export default Results;
