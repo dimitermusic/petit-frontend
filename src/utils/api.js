fd17753c87a83d1bdc4c535f21a9f33a96a5d814
SpecsNStats <specsnstats@gmail.com>
2021-11-29 10:29:11 -0700

    changes

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,6 +5,9 @@ const URL_PREFIX = "http://localhost:3001"
 // const URL_PREFIX = "HEROKU BACK END URL"
 
 const API = {
+    getProfile: (tkn)=>{
+        return axios.get(`${URL_PREFIX}/profile`,{headers:{"Authorization":`Bearer ${tkn}`}})
+    },
     login:(usrData)=>{
         return axios.post(`${URL_PREFIX}/api/users/login`,usrData)
     },
