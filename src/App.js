8007958e7097068a34be7eb03c2aac337bfc1a66
SpecsNStats <specsnstats@gmail.com>
2021-11-29 11:47:12 -0700

    added routing

--- src/App.js
+++ src/App.js
@@ -6,93 +6,106 @@ import ReactDOM from "react-dom";
 import API from "./utils/api";
 import Profile from "./pages/Profile/index.js";
 import NavBar from "./components/NavBar/index.js";
+import { Routes, Route, Link } from "react-router-dom";
 const axios = require("axios");
+
 
 function App() {
 
-  const [userState,setUserState]= useState({
-    username:"",
-    id:0
+  const checkLogin = () => {
+    {
+      !userState.username ?
+        <SignupForm
+          submitSignup={handleSignupSubmit}
+          submitSignin={handleSigninSubmit}
+          change={handleLoginChange}
+          loginState={loginFormState} /> : <Profile />
+    }
+  }
+
+  const [userState, setUserState] = useState({
+    username: "",
+    id: 0
   })
-  
+
   const [token, setToken] = useState("")
 
   const [searchFormState, setSearchFormState] = useState({
     search: "",
-    city:"",
-    type:""
+    city: "",
+    type: ""
   })
 
-  const [loginFormState, setLoginFormState] = useState ({
-    usernameSignIn:"",
-    passwordSignIn:"",
-    emailSignUp:"",
-    usernameSignUp:"",
-    passwordSignUp:""
+  const [loginFormState, setLoginFormState] = useState({
+    usernameSignIn: "",
+    passwordSignIn: "",
+    emailSignUp: "",
+    usernameSignUp: "",
+    passwordSignUp: ""
   });
 
-  useEffect(()=>{
+  useEffect(() => {
     const myToken = localStorage.getItem("token")
     console.log("successfully used")
     console.log(myToken)
 
-    if(myToken){
+    if (myToken) {
       API.getProfile(myToken)
-      .then(res=>{
-        console.log("successfully obtained token!")
-        setToken(myToken)
-        setUserState({
-          username: res.data.username,
-          id: res.data.id
+        .then(res => {
+          console.log("successfully obtained token!")
+          setToken(myToken)
+          setUserState({
+            username: res.data.username,
+            id: res.data.id
+          })
+        }).catch(err => {
+          console.log("whoops")
+          console.log(err)
+          localStorage.removeItem("token")
         })
-      }).catch(err=>{
-        console.log("whoops")
-        console.log(err)
-        localStorage.removeItem("token")
+    }
+  }, [])
+
+  const handleSearchChange = event => {
+    console.log(event.target.value)
+    if (event.target.name === "search") {
+      setSearchFormState({
+        ...searchFormState,
+        search: event.target.value
+      })
+    } else if (event.target.name === "city") {
+      setSearchFormState({
+        ...searchFormState,
+        city: event.target.value
+      })
+    } else {
+      setSearchFormState({
+        ...searchFormState,
+        type: event.target.value
       })
     }
-  },[])
-
-  const handleSearchChange= event =>{
-    console.log(event.target.value)
-  if(event.target.name === "search"){
-    setSearchFormState({
-      ...searchFormState,
-      search: event.target.value
-    })
-  } else if (event.target.name === "city") {
-      setSearchFormState({
-          ...searchFormState,
-          city: event.target.value
-      })
-  } else {
-      setSearchFormState({
-          ...searchFormState,
-          type: event.target.value  
-      })
   }
-}
 
-  const handleLoginChange = event=>{
-    if(event.target.name==="usernameSignIn"){
+  const handleLoginChange = event => {
+    if (event.target.name === "usernameSignIn") {
       setLoginFormState({
         ...loginFormState,
         usernameSignIn: event.target.value
       });
-    } else if (event.target.name==="passwordSignIn"){
+    } else if (event.target.name === "passwordSignIn") {
       setLoginFormState({
         ...loginFormState,
-        passwordSignIn:event.target.value
+        passwordSignIn: event.target.value
       });
-    } else if (event.target.name==="emailSignUp"){
+    } else if (event.target.name === "emailSignUp") {
       setLoginFormState({
         ...loginFormState,
-        emailSignUp:event.target.value
+        emailSignUp: event.target.value
       });
-    } else if (event.target.name==="usernameSignUp"){
+    } else if (event.target.name === "usernameSignUp") {
       setLoginFormState({
         ...loginFormState,
-        usernameSignUp:event.target.value
+        usernameSignUp: event.target.value
       });
     } else {
       setLoginFormState({
@@ -102,26 +115,26 @@ function App() {
     }
   }
 
-  const handleSigninSubmit = event=>{
+  const handleSigninSubmit = event => {
     event.preventDefault();
     API.login({
       username: loginFormState.usernameSignIn,
       password: loginFormState.passwordSignIn
     })
-    .then(res=>{
-      console.log(res.data)
-      setUserState({
-        username: res.data.username,
-        id: res.data.id
+      .then(res => {
+        console.log(res.data)
+        setUserState({
+          username: res.data.username,
+          id: res.data.id
+        })
+        setToken(res.data.token)
+        localStorage.setItem("token", res.data.token)
+      }).catch(err => {
+        console.log(err);
       })
-      setToken(res.data.token)
-      localStorage.setItem("token", res.data.token)
-    }).catch(err=>{
-      console.log(err);
-    })
   }
 
-  const handleSignupSubmit = event=>{
+  const handleSignupSubmit = event => {
     // event.preventDefault();
     console.log("event is triggered")
     API.signup({
@@ -129,55 +142,52 @@ function App() {
       username: loginFormState.usernameSignUp,
       password: loginFormState.passwordSignUp
     })
-    .then(res=>{
-      console.log("response is received")
-      API.login({
-        username:loginFormState.usernameSignUp,
-        password:loginFormState.passwordSignUp
+      .then(res => {
+        console.log("response is received")
+        API.login({
+          username: loginFormState.usernameSignUp,
+          password: loginFormState.passwordSignUp
+        })
+        console.log(res.data)
+        setUserState({
+          username: res.data.username,
+          id: res.data.id
+        })
+        setToken(res.data.token)
+        localStorage.setItem("token", res.data.token)
+      }).catch(err => {
+        console.log(err);
       })
-      console.log(res.data)
-      setUserState({
-        username:res.data.username,
-        id:res.data.id
-      })
-      setToken(res.data.token)
-      localStorage.setItem("token",res.data.token)
-    }).catch(err=>{
-      console.log(err);
-    })
   }
 
-  const logOut=()=>{
-    setUserState({username:"", id:0})
+  const logOut = () => {
+    setUserState({ username: "", id: 0 })
     setToken("")
     localStorage.removeItem("token")
   }
 
+  function LoginPage() {
+     return (!userState.username ?
+      <SignupForm
+        submitSignup={handleSignupSubmit}
+        submitSignin={handleSigninSubmit}
+        change={handleLoginChange}
+        loginState={loginFormState} /> : <Profile />)
+    
+  }
+
   return (
-    <> 
-  {/* // :<h1>==========NavBar==========</h1> */}
-  <NavBar/>
-       
-  {/* <h1>==========SearchBar==========</h1> */}
-    <SearchBar
+    <>
+      <NavBar />
+
+      <SearchBar
         searchState={searchFormState}
         change={handleSearchChange}
-        />
-  
-  {!userState.username?
-  // <h1>==========Login==========</h1>
-      <SignupForm 
-        submitSignup={handleSignupSubmit} 
-        submitSignin={handleSigninSubmit} 
-        change={handleLoginChange} 
-        loginState={loginFormState}/>:<Profile/>}
-
-  {/* // <h1>==========Discover==========</h1> */}
-  {/* //     <Discover/> */}
-
-  {/* // :<h1>==========Profile==========</h1> */}
- 
-  {/* // <h1>==========Edit Profile==========</h1> */}
+      />
+      <Routes>
+        {console.log(LoginPage)}
+        <Route exact path={"/"} element={<LoginPage/>}/>
+      </Routes>
     </>
   );
 }
