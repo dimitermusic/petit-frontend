5c5e48d1b3e09b6f209970faa33aeff9408b6c53
kelliekumasaka <kelliek3@gmail.com>
2021-11-30 10:33:12 -0800

    page routes

--- src/components/Results/index.js
+++ src/components/Results/index.js
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import { Routes, Route, Link, Navigate } from "react-router-dom";
+import ReactDOM from "react-dom";
+
 
 function Results(props){
     return(
@@ -7,7 +10,7 @@ function Results(props){
                 <h1 className="uk-heading-divider uk-text-center">Search Results</h1>
                 <ul className="uk-list uk-list-large uk-list-divider uk-list-striped" id='search-results'>
                     {props.places.map(place=>{
-                        return <li key={place.reference} id={place.reference}><a>{place.name} at {place.formatted_address}</a></li>
+                        return (<li key={place.reference} id={place.reference}><a href={`/review`}>{place.name} at {place.formatted_address}</a></li>)
                     })}
                 </ul>
             </div>
