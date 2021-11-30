13009f5eb18daeb016f80105e874d532aeaaac21
kelliekumasaka <kelliek3@gmail.com>
2021-11-30 14:22:44 -0800

    discover route

--- src/components/pages/Discover/index.js
+++ src/components/pages/Discover/index.js
@@ -1,8 +1,20 @@
 import React, { useState } from "react";
-import PropTypes from 'prop-types';
 import style from "./style.css"
+import API from "../../../utils/api";
 
 function Discover(props) {
+    const [places, setPlaces] = useState([])
+
+    const randomPlaces = (e) =>{
+        e.preventDefault();
+        API.getAllPlaces()
+        .then(data=>{
+            setPlaces(data)
+        })
+        .catch(err=>{
+            console.log(err);
+        })
+    }
 
     return (
         <div>
