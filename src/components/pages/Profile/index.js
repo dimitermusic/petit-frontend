81437a95d2674e3bfc3772cf15d3eb03dc0b7988
Charlotte Hulseman <chulseman@fordham.edu>
2021-12-01 15:49:59 -0500

    pet gallery button on profile now functional

--- src/components/pages/Profile/index.js
+++ src/components/pages/Profile/index.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
+import API from '../../../utils/api.js'
 import style from "./style.css"
 import avatarImg from "./../../../images/avatar.jpg"
 import { Routes, Route, Link, Navigate } from "react-router-dom";
+import App from "../../../App.js"
 
 
 function Profile(props) {
@@ -11,11 +13,17 @@ if(!props.username){
     <Navigate to="/logout"/>
 }
 
+// const petGalleryButton = () => {
+//     <Navigate to="/petgallery"/>
+// }
+
     return (
          <div>
             <h1 className="uk-flex uk-flex-center">My Profile</h1>
 
         <p className="uk-text-bold uk-text-small uk-flex uk-flex-center ">@{props.username}</p>
+
+        <p className="uk-text-bold uk-text-small uk-flex uk-flex-center ">Votes: 33{props.votes}</p>
 
         <img src={avatarImg} width="200" alt="avatar" className="uk-img uk-placeholder uk-align-center"></img>
 
@@ -29,9 +37,9 @@ if(!props.username){
 
             {/* Profile nav */}
             <ul className="uk-subnav uk-subnav-divider uk-margin">
-                    <li className="uk-active"><a href="#"><span uk-icon="file-text"></span> Comments</a></li>
-                    <li><a href="#"><span uk-icon="image"></span> Pet Gallery</a></li>
-                    <li><a href="#"><span uk-icon="comment"></span> Reviews</a></li>
+                    <li className="uk-active"><a href="#"><span uk-icon="file-text"></span> Reviews</a></li>
+                    <li><a href="/petgallery"><span uk-icon="image"></span> Pet Gallery</a></li>
+                    <li><a href="#"><span uk-icon="comment"></span> Comments</a></li>
             </ul>
             {/* Populate user reviews */}
             <div   className="uk-child-width-expand@s uk-text-center uk-grid">
