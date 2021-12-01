0415d1d377af9ff2ec61b19bba75748f83a9b5c4
kelliekumasaka <kelliek3@gmail.com>
2021-12-01 09:30:13 -0800

    changes

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,16 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import API from '../../utils/api'
 import Results from "../Results";
-import { Routes, Route, Link, Navigate } from "react-router-dom";
-
+import {useSelector,useDispatch} from "react-redux";
 
 function SearchBar(props) {
-    const [searchFormState, setSearchFormState] = useState({
-        search: "",
-        city: "",
-        type: "establishment"
-    })
-    const [result, setResult] = useState([]);
+    // const [searchFormState, setSearchFormState] = useState({
+    //     search: "",
+    //     city: "",
+    //     type: "establishment"
+    // })
+    // const [result, setResult] = useState([]);
+    const dispatch = useDispatch();
+    const searchForm = useSelector(state => state.searchForm)
 
     const handleSearchChange = event => {
         if (event.target.name === "search") {
@@ -49,16 +50,16 @@ function SearchBar(props) {
         <>
             <div className="uk-flex uk-flex-center uk-margin">
                 <form className="uk-flex-inline uk-search">
-                    <input name="search" id="estName" className="uk-search-input" type="search" value={searchFormState.search} onChange={handleSearchChange} placeholder="Name" />
-                    <input name="city" id="city" className="uk-search-input" type="search" value={searchFormState.city} onChange={handleSearchChange} placeholder="City" />
-                    <select className="uk-select" id="form-stacked-select" name="type" value={searchFormState.type} onChange={handleSearchChange}>
+                    <input name="search" id="estName" className="uk-search-input" type="search" value={searchForm.search} onChange={handleSearchChange} placeholder="Name" />
+                    <input name="city" id="city" className="uk-search-input" type="search" value={searchForm.city} onChange={handleSearchChange} placeholder="City" />
+                    <select className="uk-select" id="form-stacked-select" name="type" value={searchForm.type} onChange={handleSearchChange}>
                         <option value='establishment'>Establishment</option>
                         <option value='job'>Job</option>
                     </select>
                 </form>
                 <button className="uk-button uk-button-default" onClick={apiFetch}>Button</button>
             </div>
-            <Results places={result} type={searchFormState.type} />
+            {/* <Results places={result} type={searchFormState.type} /> */}
         </>
     )
 }
