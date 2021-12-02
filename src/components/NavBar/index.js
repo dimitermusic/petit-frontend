048df56c0d7933be6b95931281f07d1d3320fb33
dimitermusic <dimiteryyordanov@gmail.com>
2021-12-01 22:51:32 -0800

    removed unwanted profile components and began styling navbar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,29 +1,30 @@
 import React, { useState } from "react";
 import PropTypes from 'prop-types';
 import { getThemeProps } from "@mui/system"
+import './style.css'
 
 function NavBar(props) {
 
     return (
         <>
             <div>
-                <nav className="uk-navbar-container" uk-navbar="true">
-
-                    <div className="uk-navbar-right">
-                        <a className="uk-navbar-item uk-logo" href="/discover">PETIT | LOGO</a>
-                
-                    <div className="uk-navbar-right">
-                        <ul className="uk-navbar-nav">
-                            <li className="uk-active">
+                <nav className="uk-navbar-container navbar" uk-navbar="true">
+                    <div>
+                        <a className="uk-navbar-item uk-logo nav-logo" href="/discover">PETIT | LOGO</a>
+                    </div>
+                    <div>
+                        <ul className="uk-navbar-nav nav-items">
+                            <li>
                                 <a href="/discover">Discover</a>
                             </li>
                             <li>
                                 <a href={`/profile`}>Profile</a>
                             </li>
-                            <li><a href="/logout">Logout</a></li>
+                            <li>
+                                <a href="/logout">Login/Logout</a>
+                            </li>
                         </ul>
                     </div>
-                 </div>
                 </nav>
             </div>
         </>
